b3a00a84deb2dd190f89c1dd099bbf3bfa1f1583
Leonardo <jaques.projetos@outlook.com>
2018-12-14 13:57:00 -0200

    Creating Observales-1

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-alert('Hello RxJS!!!');
+ import { Observable } from 'rxjs';
+ import { allBooks } from "./data";
+
+
+
+ let allBooksObservable$ = Observable.create(subscriber => {
+
+     if (document.title !== 'RxBookTracker'){
+         subscriber.error('Incorrect page title.');
+     }
+
+     for (let book of allBooks){
+         subscriber.next(book);
+     }
+
+     setTimeout(()=>{
+         subscriber.complete();
+     },2000);
+
+     return () => console.log('Executing teardown code.')
+ });
+
+ allBooksObservable$.subscribe(book => console.log(book.title));
